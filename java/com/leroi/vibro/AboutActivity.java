799036c784d65d6a9ee6ed87f09aeadc50636f0f
David Uche <uchedavy@gmail.com>
2016-07-31 14:34:44 +0300

    Removed some auto generated comments

--- java/com/leroi/vibro/AboutActivity.java
+++ java/com/leroi/vibro/AboutActivity.java
@@ -13,18 +13,18 @@ public class AboutActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
     }
+    
+    
 
 
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+      
         if (id == R.id.action_settings) {
             return true;
         }
