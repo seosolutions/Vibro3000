e19925f61932c2b885ba35ecbe0d146b4e47c5b4
David Uche <uchedavy@gmail.com>
2016-08-02 14:18:56 +0300

    Restricted vibration access

--- java/com/leroi/vibro/BoredActivity.java
+++ java/com/leroi/vibro/BoredActivity.java
@@ -46,9 +46,7 @@ public class BoredActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+  
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -62,7 +60,8 @@ public class BoredActivity extends Activity {
     /*
     * This prevents the app from crashing when the back button is clicked. Also stops the vibrator.
     * */
-    public void onStop(){
+    //RESTRICT ACCESS
+    private void onStop(){
         myVib.cancel();
         this.onStart();
     }
